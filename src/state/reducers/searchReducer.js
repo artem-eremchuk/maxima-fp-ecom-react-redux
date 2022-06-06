f4f9971b2167db54e3b7dcc392f66a0c1b9421b3
Artem Eremchuk <eremchuk.it@gmail.com>
2022-06-06 09:14:07 +0300

    fix search function

--- src/state/reducers/searchReducer.js
+++ src/state/reducers/searchReducer.js
@@ -3,7 +3,7 @@ const SEARCH_PRODUCT = 'SEARCH_PRODUCT';
 const searchReducer = (state = '', action) => {
   switch(action.type) {
     case SEARCH_PRODUCT:
-      return action.payload;
+      return action.payload.toLowerCase();
     default:
       return state;
   }
